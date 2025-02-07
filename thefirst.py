418d50b081a0d437df226d5fbe5893cfca358e3f
diogofariasz <ediogo846@gmail.com>
2025-02-07 09:09:05 -0300

    estudos

---
+++ thefirst.py
@@ -0,0 +1,27 @@
+print ("hello world")
+
+idade = 30
+nota = 8.5
+nome = "diogo farias"
+instituto ='ifrn'
+passou = True
+maior = False
+
+print (nome)
+print("a idade é:", idade)
+
+nota_1 = 8
+nota_2 = 9
+
+soma = nota_1 + nota_2
+print("o valor é:", soma)
+
+media = (nota_1 + nota_2) /2
+print("o valor é:", media)
+
+print(10+3)
+
+print(10/3)
+print(10//3)
+
+print(10**3)
