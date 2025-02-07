f063e2298f5658cf401838e12b7fe6b8b5390ac2
diogofariasz <ediogo846@gmail.com>
2025-02-07 10:04:36 -0300

    estudos

---
+++ thesecond.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+print("o nome é:", nome)
+
+idade = int(input("Digite sua idade: "))
+print("idade é:", idade)
+
+nota = float(input("Digite sua nota: "))
+print("nota é:", nota)
