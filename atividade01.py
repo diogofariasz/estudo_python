107448a7a15359b9b0975b85e314795bbf0bbffd
diogofariasz <ediogo846@gmail.com>
2025-02-07 09:57:48 -0300

    estudos

---
+++ atividade01.py
@@ -0,0 +1,56 @@
+#atividade01
+print("É preciso fazer todos os algoritmos", 
+      "para aprender Python!!")
+
+#atividade 02
+ifrn = "Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Norte"
+print(ifrn)
+
+#atividade 03
+numero = 20
+print("o numero é:", numero)
+
+#atividade 04
+numero = 30 * 2
+print("o numero é:", numero)
+
+#atividade 05
+numerador = 1
+denominador = 2
+
+fracao = numerador / denominador
+print("o numero é:", fracao)
+
+#atividade 06
+peso = 108
+altura = 1.80
+
+imc= peso / (altura*2)
+print("seu imc é:", imc)
+
+#atividade 07
+nota1 = 7
+nota2 = 5
+nota3 = 9
+
+media = (nota1 + nota2 + nota3) / 3
+print("a média é:",media)
+
+#atividade 08
+valor = 100 * 0.15
+print("o valor é:",valor)
+
+#atividade 09
+venda = float(input("Digite o valor do produto: "))
+
+desconto = valor * 0.15
+resultado = valor - desconto
+
+print(f"o valor é: R$ {resultado:.2f}")
+
+#atividade 10
+salario_minimo = 1.518
+seu_salario = float(input("Digite o valor do seu salário: "))
+
+valor = seu_salario / salario_minimo
+print(f"Você recebe: R$  {valor:.2f} salários mínimos.")
