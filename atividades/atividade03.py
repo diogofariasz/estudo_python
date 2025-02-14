a17d49bfca17afb72a26ee5344ac619f3ba1c61c
diogofariasz <ediogo846@gmail.com>
2025-02-14 10:00:42 -0300

    atividade 03

---
+++ atividades/atividade03.py
@@ -0,0 +1,46 @@
+#atividade 1
+numero = int(input("digite um numero: "))
+valor = numero > 0
+print(valor)
+
+#atividade 2
+numero = float(input("digite um numero: "))
+valor = numero % 2 == 0
+print(valor)
+
+#atividade 3
+numero = float(input("digite um numero: "))
+valor = numero % 3 == 0
+print(valor)
+
+#atividade 4
+numero = int(input("digite um numero: "))
+valor = (5 <= numero < 50)
+print(valor)
+
+#atividade 5
+numero = int(input("digite um numero: "))
+valor = (0 <= numero < 10)
+print(valor)
+
+#atividade 6
+numero = int(input("digite um numero: "))
+valor = (-10 <= numero < 10)
+print(valor)
+
+#atividade 7
+numero = int(input("digite um numero: "))
+print(numero > 0  and numero % 2 ==0)
+
+#atividade 8
+numero = int(input("digite um numero: "))
+valor = (10 <= numero < 20)
+print(valor)
+
+#atividade 9
+numero = int(input("digite um numero: "))
+print(numero >= 10  and numero % 2 ==0)
+
+#atividade 10
+numero = int(input("digite um numero: "))
+print(10 <= numero <= 20 and numero % 2 ==0)
---
+++ thethird.py
@@ -0,0 +1,29 @@
+'''
+Operadores condicionais
+
+>  maior
+< menor
+>= maior ou igual
+<= menor ou igual
+== igual
+!= diferente
+'''
+
+print(5>3)
+print(5<3)
+print(5>=3)
+print(5<=3)
+print(5==3)
+print(5!=3)
+
+'''
+Valores Logicos
+
+and - e
+or - ou
+not - não
+'''
+
+print((10>5) and (5!=5))
+print((10>5) or (5!=5))
+print(not(10>5))
